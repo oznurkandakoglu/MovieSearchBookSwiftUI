7eaf7f21be54e8cfe136ec6718510f898bc3b7a6
Öznur Kandakoğlu <oznurkandakoglu@gmail.com>
2023-05-11 10:24:10 +0300

    Initial Commit

---
+++ MovieSearchBookSwiftUI/MovieSearchBookSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearchBookSwiftUIApp.swift
+//  MovieSearchBookSwiftUI
+//
+//  Created by Tiga  on 11.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieSearchBookSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
